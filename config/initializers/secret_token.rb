4f5bb2cbabec4b2f1321181f6cacfce70f040c75
Uku Täht <uku.taht@gmail.com>
2013-03-05 13:14:37 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterJr::Application.config.secret_token = '23fa8433ad71dc871d2a3686ea67baaf93359e431808cce2e084d34ca1607a58abee9b4d26841cb7d180d69c84b9142c6904aef2ef0d0a7e47d387f40d095df0'
