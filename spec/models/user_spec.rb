98fa81f44c7df0f3ae06a3ce1f8495ac3b566cc9
Uku Täht <uku.taht@gmail.com>
2013-03-08 16:04:57 +0200

    feed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,6 +30,12 @@ describe User do
   it { should respond_to(:authenticate) }
   it { should respond_to(:microposts) }
   it { should respond_to(:feed) }
+  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }  
+  it { should respond_to(:following?) }
+  it { should respond_to(:follow!) }
+  it { should respond_to(:followers) }
+
   it { should be_valid }
   it { should_not be_admin }
 
@@ -123,6 +129,30 @@ describe User do
   		before { @user.password = @user.password_confirmation = "a" * 5 }
   		it { should be_invalid }
 	end
+
+  describe "following" do
+    let(:other_user) { FactoryGirl.create(:user) }    
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
+
+    it { should be_following(other_user) }
+    its(:followed_users) { should include(other_user) }
+
+    describe "followed user" do
+      subject { other_user }
+      its(:followers) { should include(@user) }
+    end
+
+     describe "and unfollowing" do
+      before { @user.unfollow!(other_user) }
+
+      it { should_not be_following(other_user) }
+      its(:followed_users) { should_not include(other_user) }
+    end
+  end
+
 
   describe "remember token" do
     before { @user.save }
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,27 +1,34 @@
 require 'spec_helper'
 
-describe "Static pages" do
+  describe "Home page" do
 
-   subject { page }
-
-  describe "for signed-in users" do
+    
+    describe "for signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
       before do
-        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
-        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        FactoryGirl.create(:micropost, user: user, content: "Lorem")
+        FactoryGirl.create(:micropost, user: user, content: "Ipsum")
         sign_in user
         visit root_path
-      end 
+      end
 
-    it { should have_selector('h1',    text: 'Twitter Junior') }
-    it { should have_selector('title', text: full_title('')) }
-    it { should_not have_selector 'title', text: '| Home' }
-
-    it "should render the user's feed" do
+      it "should render the user's feed" do
         user.feed.each do |item|
           page.should have_selector("li##{item.id}", text: item.content)
         end
       end
+
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
+      end
+    end
   end
 
 
